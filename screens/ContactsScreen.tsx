8bbfb6a7eaeb6a050a6accb74d4dce8474650884
Ubong Eduok <ubongeduok@ymail.com>
2021-07-22 16:02:21 +0100

    display users from the aws api

--- screens/ContactsScreen.tsx
+++ screens/ContactsScreen.tsx
@@ -2,11 +2,33 @@ import * as React from 'react';
 import { StyleSheet, FlatList } from 'react-native';
 import NewMessageButton from '../components/NewMessageButton'
 import {  View } from '../components/Themed';
-import users from '../data/Users'
 import ContactsListItem from '../components/ContactsList';
-
+import { useEffect, useState } from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
+import { listUsers } from '../src/graphql/queries';
 
 export default function ContactsScreen() {
+
+  const [ users, setUsers] = useState([])
+  
+  useEffect(() =>{
+    const fetchUser = async () => {
+      try {
+        const usersData = await API.graphql(
+          graphqlOperation(
+            listUsers
+          )
+        )
+        // update user states 
+        setUsers(usersData.data.listUsers.items)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchUser()
+  }, [])
+
+
   return (
     <View style={styles.container}>
       <FlatList style={{ width: '100%'}}  data={users}
